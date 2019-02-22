95bd6fb571b6e50d356c6d9c632b97ed05387a07
oooofeiger <101457769@qq.com>
2019-02-22 17:27:07 +0800

    login & register

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,12 +1,38 @@
 import React from 'react';
-
+import { List, InputItem, WingBlank, WhiteSpace, Button } from 'antd-mobile';
+import { connect } from 'dva';
+import styles from './style.less';
+import Logo from '@/components/logo';
 
 class Login extends React.Component{
+    constructor(props){
+        super(props);
 
+        this.register = this.register.bind(this);
+    }
+
+    register = () => {
+        this.props.history.push('/register')
+    }
 
     render(){
         return (
-            <div></div>
+            <div>
+                <Logo />
+                <h2>登录页</h2>
+                
+                <WingBlank>
+                    <List>
+                        <InputItem>用户</InputItem>
+                        <WhiteSpace />
+                        <InputItem>密码</InputItem>
+                    </List>
+                    <WhiteSpace />
+                    <Button type="primary">登录</Button>
+                    <WhiteSpace />
+                    <Button type="primary" onClick={this.register}>注册</Button>
+                </WingBlank>
+            </div>
         )
     }
 }
--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -1,12 +1,44 @@
 import React from 'react';
+import { List, InputItem, WingBlank, WhiteSpace, Button, Radio } from 'antd-mobile';
+import { connect } from 'dva';
+import styles from './style.less';
+import Logo from '@/components/logo';
 
+const RadioItem = Radio.RadioItem;
 
 class Register extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            type: 'genius',
+        }
+        this.register = this.register.bind(this);
+    }
 
+    register = () => {
+        this.props.history.push('/login')
+    }
+    
 
     render(){
+        const { type } = this.state;
         return (
-            <div></div>
+            <div>
+                <Logo />
+                <h2>注册页</h2>
+                <List>
+                    <InputItem>用户名</InputItem>
+                    <InputItem>密码</InputItem>
+                    <InputItem>确认密码</InputItem>
+                    <RadioItem checked={type==='genius'}>牛人</RadioItem>
+                    <RadioItem checked={type==='boss'}>BOSS</RadioItem>
+                </List>
+                <WingBlank>
+                    <Button type="primary">登录</Button>
+                    <WhiteSpace />
+                    <Button type="primary" onClick={this.register}>注册</Button>
+                </WingBlank>
+            </div>
         )
     }
 }
