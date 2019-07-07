88879660e33b1b6045b38743accd4ca830c4c580
oooofeiger <1014577169@qq.com>
2019-07-07 18:38:21 +0800

    消息页面

--- src/pages/Accout/Msg/index.js
+++ src/pages/Accout/Msg/index.js
@@ -1,12 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { connect } from 'dva';
+import { List } from 'antd-mobile';
 
-
+const Item = List.Item;
+const Brief = Item.Brief;
+@connect(({chat}) => ({chatMsg: chat.chatMsg}))
 class Msg extends React.Component{
 
 
     render(){
+        const { chatMsg } = this.props;
+        let msgGroup = {};
+        chatMsg.forEach(item => {
+            msgGroup[item.chatId] = msgGroup[item.chatId] || [];
+            msgGroup[item.chatId].push(item);
+        });
+
+        const chatList = chatMsg.length ? Object.values(msgGroup) : [];//将msgGroup中的value拿出来组成数组
+        console.log(Object.values(msgGroup),'msg msgGroup')
         return(
-            <div>Msg</div>
+            <div>
+                <List>
+                    <Item></Item>
+                </List>
+            </div>
         )
     }
 }
